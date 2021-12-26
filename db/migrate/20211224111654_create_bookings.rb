a8844256b91a7e2dff457ee1ab7373c2d36d5e25
Paul-Henri Eichhorn <paul.henri.eichhorn@gmail.com>
2021-12-26 18:18:45 +0100

    signup changed (with migrations)

--- db/migrate/20211224111654_create_bookings.rb
+++ db/migrate/20211224111654_create_bookings.rb
@@ -3,7 +3,6 @@ class CreateBookings < ActiveRecord::Migration[6.1]
     create_table :bookings do |t|
       t.references :listing, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-
       t.timestamps
     end
   end
