980cc64be5c237740e7d7e4ae2f22c16331dda7e
Paul-Henri Eichhorn <paul.henri.eichhorn@gmail.com>
2022-02-04 23:17:58 +0100

    dashboard in booking index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # get "dashboard", to: 'pages#dashboard', as: :dashboard
   # patch "accept", to: 'bookings#accept', as: :accept
   # patch "reject", to: 'bookings#reject', as: :reject
   resources :listings do
     resources :bookings, only: [:new, :create]
   end
+  resources :bookings, only: [:index]
   get "about-us", to: 'pages#aboutus', as: :aboutus
   get "faq", to: 'pages#faq', as: :faq
   get "work", to: 'pages#work', as: :work
