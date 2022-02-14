abce4596c878bad673998aa3c2a4cb8dc9ba8927
Paul-Henri Eichhorn <paul.henri.eichhorn@gmail.com>
2022-02-14 18:45:23 +0100

    multi image upload and show changes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,11 @@
 class Listing < ApplicationRecord
   belongs_to :user
-  has_many :bookings
-  has_one_attached :photo
+  has_many :bookings, dependent: :destroy
+  has_one_attached :photo1
+  has_one_attached :photo2
+  has_one_attached :photo3
+  has_one_attached :photo4
+  has_one_attached :photo5
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 end
