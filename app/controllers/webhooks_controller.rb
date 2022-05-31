5367727518df55cd120723fca42cabe6380e3748
Paul-Henri Eichhorn <paul.henri.eichhorn@gmail.com>
2022-05-31 15:44:58 +0200

    updating heroku with new webhook and credential key ...

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,32 +1,32 @@
 class WebhooksController < ApplicationController
-  # skip_before_action :authenticate_user!
-  # skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
 
-  # def create
-  #   payload = request.body.read
-  #   sig_header = request.env['HTTP_STRIPE_SIGNATURE']
-  #   event = nil
+  def create
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    event = nil
 
-  #   begin
-  #     event = Stripe::Webhook.construct_event(
-  #       payload, sig_header, Rails.application.credentials[:stripe][:webhook]
-  #     )
-  #   rescue JSON::ParserError => e
-  #     status 400
-  #     return
-  #   rescue Stripe::SignatureVerificationError => e
-  #     # Invalid signature
-  #     puts "Signature error"
-  #     p e
-  #     return
-  #   end
+    begin
+      event = Stripe::Webhook.construct_event(
+        payload, sig_header, Rails.application.credentials[:stripe][:webhook]
+      )
+    rescue JSON::ParserError => e
+      status 400
+      return
+    rescue Stripe::SignatureVerificationError => e
+      # Invalid signature
+      puts "Signature error"
+      p e
+      return
+    end
 
-  #   # Handle the event
-  #   case event.type
-  #   when 'checkout.session.completed'
-  #     return"the mistake was here"
-  #   end
+    # Handle the event
+    case event.type
+    when 'checkout.session.completed'
+      return "hello"
+    end
 
-  #   render json: { message: 'success' }
-  # end
+    render json: { message: 'success' }
+  end
 end
