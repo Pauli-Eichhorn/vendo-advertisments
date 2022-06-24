a07a746bdf281d8af404e2ab7962345bf4cde80c
Paul-Henri Eichhorn <paul.henri.eichhorn@gmail.com>
2022-06-24 13:22:25 +0200

    the webhook to change status form not paid to paid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_200023) do
+ActiveRecord::Schema.define(version: 2022_06_20_130935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 2022_05_28_200023) do
     t.string "city"
     t.string "zip"
     t.boolean "terms_accepted", default: false
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
